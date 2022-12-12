4f34483beb1863be98465cbfd409a4cd68101fd6
tjohnson-pvcc <117691369+tjohnson-pvcc@users.noreply.github.com>
2022-12-12 10:39:08 -0600

    Add files via upload

---
+++ tickets.py
@@ -0,0 +1,60 @@
+# Name: Trevor Johnson
+# Prog Purpose: This program finds the cost of movie tickets
+#   Price for one ticket: $10.99
+#   Sales tax rate: 5.5%
+
+
+
+import datetime
+
+SALES_TAX_RATE = 0.055
+PR_TICKET = 10.99
+
+
+num_tickets = 0
+subtotal = 0
+sales_tax = 0
+total = 0
+
+
+def main():
+    get_user_data()
+    perform_calculations()
+    display_results()
+
+
+def main():
+    get_user_data()
+    perform_calculations()
+    display_results()
+
+def get_user_data():
+    global num_tickets
+    num_tickets = int(input("Number of movie tickets:"))
+
+def perform_calculations():
+    global subtotal, sales_tax, total
+    subtotal = num_tickets * PR_TICKET
+    sales_tax = subtotal * SALES_TAX_RATE
+    total = subtotal + sales_tax
+
+
+def display_results():
+    print("------------------------------")
+    print('**** CINEMA HOUSE MOVIES ****')
+    print('Your neighborhood movie house')
+    print('------------------------------')
+    print('Tickets      $ ' + format(subtotal, '8,.2f'))
+    print('Sales Tax    $ ' + format(sales_tax, '8,.2f'))
+    print('Total        $ ' + format(total, '8,.2f'))
+    print('------------------------------')
+    print(str(datetime.datetime.now()))
+
+
+main()
+
+
+
+
+    
+    
